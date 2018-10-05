14fabda65fecc1ec6c83817e51cab86b58b0e867
anik1606 <anik1606@gmail.com>
2018-10-05 19:38:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@ def fizzbuzz (n)
   if n % 3 == 0
     return "Fizz"
     elsif n % 5 == 0 
-    return "Buzz"
+      return "Buzz"
     elsif n % 3 == 0 && n % 5 == 0
-    return "FizzBuzz"
+      return "FizzBuzz"
   else 
     return nil 
   end
